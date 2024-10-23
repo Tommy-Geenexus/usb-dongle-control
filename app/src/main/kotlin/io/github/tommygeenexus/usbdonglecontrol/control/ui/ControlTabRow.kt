1b451142a11bbaec9f99c92b7b21149878c2f763
Tommy-Geenexus <tomgapplicationsdevelopment@gmail.com>
2024-10-23 18:49:19 +0200

    fix(control): adapt layout for Android 15

--- app/src/main/kotlin/io/github/tommygeenexus/usbdonglecontrol/control/ui/ControlTabRow.kt
+++ app/src/main/kotlin/io/github/tommygeenexus/usbdonglecontrol/control/ui/ControlTabRow.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import io.github.tommygeenexus.usbdonglecontrol.core.control.ControlTabs
 
 @Composable
 fun ControlTabRow(
